3f11f71405302f2ee67e074b15db37adc86df6a9
Geoff Lew <admin@appearingoffline.com>
2019-03-11 22:40:09 +0000

    fixed proxy

--- passport.js
+++ passport.js
@@ -6,7 +6,7 @@ passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
-    proxy:true;
+    proxy: true
   },
   (accessToken, refreshToken, profile, done) => {
       console.log('AC', accessToken);
